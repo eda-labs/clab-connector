b4529a4b09c7c3e3fa572bd0f76940637fc1e71e
FloSch62 <schwarz.flori.88@gmail.com>
2026-02-09 14:45:20 +0100

    fix lint

--- clab_connector/clients/kubernetes/client.py
+++ clab_connector/clients/kubernetes/client.py
@@ -210,7 +210,15 @@ def edactl_namespace_bootstrap(namespace: str) -> int | None:
     """
     toolbox = get_toolbox_pod()
     core_api = k8s_client.CoreV1Api()
-    cmd = ["edactl", "namespace", "bootstrap", "create", namespace, "--from-namespace", "eda"]
+    cmd = [
+        "edactl",
+        "namespace",
+        "bootstrap",
+        "create",
+        namespace,
+        "--from-namespace",
+        "eda",
+    ]
     try:
         resp = stream(
             core_api.connect_get_namespaced_pod_exec,
